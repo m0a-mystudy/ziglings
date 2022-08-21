7656f271ddf59fafd8dd469f1f452b14dc47760f
makoto abe <abe00makoto@gmail.com>
2022-08-21 15:44:48 +0900

    add 052 - 063 translates (#5)
    * add 052 - 054 translates
    
    * add 55 translate
    
    * add 56 translate
    
    * add 57 translate
    
    * add 058 translate
    
    * add 059 translate
    
    * add 060 translate
    
    * add 061 translate
    
    * add 062 translate
    
    * add 063 translate

--- exercises_trans/ja_JP.UTF-8/054_manypointers.zig
+++ exercises_trans/ja_JP.UTF-8/054_manypointers.zig
@@ -1,54 +1,54 @@
 //
-// You can also make pointers to multiple items without using a slice.
+// スライスを使用せずに、複数のアイテムへのポインタを作成することもできます。
 //
 //     var foo: [4]u8 = [4]u8{ 1, 2, 3, 4 };
 //     var foo_slice: []u8 = foo[0..];
 //     var foo_ptr: [*]u8 = &foo;
 //
-// The difference between foo_slice and foo_ptr is that the slice has
-// a known length. The pointer doesn't. It is up to YOU to keep track
-// of the number of u8s foo_ptr points to!
+// foo_slice と foo_ptr の違いは、スライスの長さが既知であることです。
+// ポインターは長さをわかっていません
+// foo_ptr が指し示す u8 の個数を追跡するのはあなた自身となります。
 //
 const std = @import("std");
 
 pub fn main() void {
-    // Take a good look at the array type to which we're coercing
-    // the zen12 string (the REAL nature of strings will be
-    // revealed when we've learned some additional features):
+    // zen12 の文字列を強制する配列の型をよく見てください 
+    // (文字列の本当の性質は、いくつかの追加機能を学習したときに明らかになります)。
+    // 
     const zen12: *const [21]u8 = "Memory is a resource.";
     //
-    //   It would also have been valid to coerce to a slice:
+    //   スライスに強制することも有効です。:
     //         const zen12: []const u8 = "...";
     //
-    // Now let's turn this into a "many-item pointer":
+    // ここで、これを「多項目ポインタ(many-item pointer)」にしてみましょう。:
     const zen_manyptr: [*]const u8 = zen12;
 
-    // It's okay to access zen_manyptr just like an array or slice as
-    // long as you keep track of the length yourself!
+    // 配列やスライスと同様に、zen_manyptr にアクセスしてもかまいません。
+    // 長さを自分で管理していれば大丈夫です!
     //
-    // A "string" in Zig is a pointer to an array of const u8 values
-    // (or a slice of const u8 values, as we saw above). So, we could
-    // treat a "many-item pointer" of const u8 as a string as long as
-    // we can CONVERT IT TO A SLICE. (Hint: we do know the length!)
+    // Zig における「文字列」は、const u8 値の配列へのポインタです
+    // （上で見たように、const u8 値のスライスでもかまいません）。
+    // つまり、const u8 の「多項目ポインタ」は、それをスライスに変換することができれば、
+    // 文字列として扱うことができます。(ヒント: 長さはわかっている!)
     //
-    // Please fix this line so the print statement below can print it:
+    // この行を修正して、下の print 文がこれを出力できるようにしてください。
     const zen12_string: []const u8 = zen_manyptr;
 
-    // Here's the moment of truth!
+    // ここからが正念場!
     std.debug.print("{s}\n", .{zen12_string});
 }
 //
-// Are all of these pointer types starting to get confusing?
+// ポインタの型がたくさんあって、混乱してきましたか？
 //
-//     FREE ZIG POINTER CHEATSHEET! (Using u8 as the example type.)
+//     無料の ZIG POINTER チートシート (u8をサンプル型として使用)
 //   +---------------+----------------------------------------------+
-//   |  u8           |  one u8                                      |
-//   |  *u8          |  pointer to one u8                           |
-//   |  [2]u8        |  two u8s                                     |
-//   |  [*]u8        |  pointer to unknown number of u8s            |
-//   |  [*]const u8  |  pointer to unknown number of immutable u8s  |
-//   |  *[2]u8       |  pointer to an array of 2 u8s                |
-//   |  *const [2]u8 |  pointer to an immutable array of 2 u8s      |
-//   |  []u8         |  slice of u8s                                |
-//   |  []const u8   |  slice of immutable u8s                      |
+//   |  u8           |  1つのu8                                      |
+//   |  *u8          |  1つのu8へのポインタ                            | 
+//   |  [2]u8        |  u8を2つ                                      |
+//   |  [*]u8        |  未知個数の u8 のポインタ                        |
+//   |  [*]const u8  |  不変の u8 の未知個数のポインタ                   |
+//   |  *[2]u8       |  2個のu8からなる配列へのポインタ                  |
+//   |  *const [2]u8 |  2個のu8からなる不変の配列へのポインタ             |
+//   |  []u8         |  u8のスライス                                  |
+//   |  []const u8   |  不変の u8 のスライス                           |
 //   +---------------+----------------------------------------------+
--- exercises_trans/ja_JP.UTF-8/061_coercions.zig
+++ exercises_trans/ja_JP.UTF-8/061_coercions.zig
@@ -1,14 +1,14 @@
 //
-// It'll only take us a moment to learn the Zig type coercion
-// rules because they're quite logical.
+// Zig型の強制適用を学ぶには、ほんの少し時間がかかります。
+// ルールは非常に論理的だからです。
 //
-// 1. Types can always be made _more_ restrictive.
+// 1. 型は常に「より」制限的にすることができる。
 //
 //    var foo: u8 = 5;
 //    var p1: *u8 = &foo;
-//    var p2: *const u8 = p1; // mutable to immutable
+//    var p2: *const u8 = p1; // ミュータブルからイミュータブルへ
 //
-// 2. Numeric types can coerce to _larger_ types.
+// 2. 数値型はより「大きな」型に変換することができる
 //
 //    var n1: u8 = 5;
 //    var n2: u16 = n1; // integer "widening"
@@ -16,51 +16,51 @@
 //    var n3: f16 = 42.0;
 //    var n4: f32 = n3; // float "widening"
 //
-// 3. Single-item pointers to arrays coerce to slices and
-//    many-item pointers.
+// 3. 配列への単一項目ポインタは、スライスと多項目ポインタに変換される。
+// 
 //
 //    const arr: [3]u8 = [3]u8{5, 6, 7};
 //    const s: []const u8 = &arr;  // to slice
 //    const p: [*]const u8 = &arr; // to many-item pointer
 //
-// 4. Single-item mutable pointers can coerce to single-item
-//    pointers pointing to an array of length 1. (Interesting!)
+// 4. 単項目ポインタは、長さ1の配列を指す単項目ポインタに変換することができます。
+// 
 //
 //    var five: u8 = 5;
 //    var a_five: *[1]u8 = &five;
 //
-// 5. Payload types and null coerce to optionals.
+// 5. ペイロード型と NULL強制をオプショナルに 
 //
 //    var num: u8 = 5;
 //    var maybe_num: ?u8 = num; // payload type
 //    maybe_num = null;         // null
 //
-// 6. Payload types and errors coerce to error unions.
+// 6. ペイロード型とエラーは、エラーユニオンに強制される。
 //
 //    const MyError = error{Argh};
 //    var char: u8 = 'x';
 //    var char_or_die: MyError!u8 = char; // payload type
 //    char_or_die = MyError.Argh;         // error
 //
-// 7. 'undefined' coerces to any type (or it wouldn't work!)
+// 7. 'undefined' は任意の型に強制される (そうでなければ動作しない!)。
 //
-// 8. Compile-time numbers coerce to compatible types.
+// 8. コンパイル時の数値は互換性のある型に強制される。
 //
-//    Just about every single exercise program has had an example
-//    of this, but a full and proper explanation is coming your
-//    way soon in the third-eye-opening subject of comptime.
+// しかし、完全で適切な説明は、
+// 第三の目を開くテーマである comptime で近々行われる予定です。
+// 
 //
-// 9. Tagged unions coerce to the current tagged enum.
+// 9. タグ付きユニオンは、現在のタグ付き列挙型を強制する 
 //
-// 10. Enums coerce to a tagged union when that tagged field is a
-//     zero-length type that has only one value (like void).
+// 10. 列挙型は、タグ付きフィールドが (void のような) 値を 1 つだけ持つ長さゼロの型である場合、
+// 　　列挙型はタグ付きユニオンになる。
 //
-// 11. Zero-bit types (like void) can be coerced into single-item
-//     pointers.
+// 11. ゼロビット型（voidなど）は1項目ポインタに強制することができる。
+// 
 //
-// The last three are fairly esoteric, but you're more than
-// welcome to read more about them in the official Zig language
-// documentation and write your own experiments.
+// 最後の3つはかなり難解ですが、詳しくはZig言語の公式ドキュメントを読んで、
+// 自分で実験してみるといいでしょう。
+// 
 
 const print = @import("std").debug.print;
 
@@ -69,10 +69,10 @@ pub fn main() void {
 
     const my_letter:   ???   = &letter;
     //               ^^^^^^^
-    //           Your type here.
-    // Must coerce from &letter (which is a *u8).
-    // Hint: Use coercion Rules 4 and 5.
+    //        ここにあなたの型があります。
+    // &letter (これは *u8) から強制されなければなりません。
+    // ヒント：強制ルール4と5を使用する。
 
-    // When it's right, this will work:
+    // 正解の場合、これは動作します。
     print("Letter: {u}\n", .{my_letter.?.*[0]});
 }
