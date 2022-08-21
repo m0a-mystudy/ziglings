e482cb86007f6d92ae592dd7072d7b14674eb3e4
makoto abe <abe00makoto@gmail.com>
2022-08-21 15:29:51 +0900

    add 062 translate

--- exercises_trans/ja_JP.UTF-8/062_loop_expressions.zig
+++ exercises_trans/ja_JP.UTF-8/062_loop_expressions.zig
@@ -1,35 +1,35 @@
 //
-// Remember using if/else statements as expressions like this?
+// if/else文をこのような式として使用したことを覚えていますか？
 //
 //     var foo: u8 = if (true) 5 else 0;
 //
-// Zig also lets you use for and while loops as expressions.
+// Zig では、for や while ループも式として使用できます。
 //
-// Like 'return' for functions, you can return a value from a
-// loop block with break:
+// 関数の 'return' のように、
+// break を使ってループブロックから値を返すことができる。
 //
-//     break true; // return boolean value from block
+//     break true; // ブロックからブール値を返す
 //
-// But what value is returned from a loop if a break statement is
-// never reached? We need a default expression. Thankfully, Zig
-// loops also have 'else' clauses! As you might have guessed, the
-// 'else' clause is evaluated when: 1) a 'while' condition becomes
-// false or 2) a 'for' loop runs out of items.
+// しかし、break 文に到達しない場合、ループからどのような値が返されるのでしょうか？
+// デフォルトの式が必要だ。
+// ありがたいことに、Zigループには「else」句があります。ご想像の通り、
+// 'else'節は次のような場合に評価されます。1) 'while' 条件が偽になったとき、
+// 2) 'for' ループがアイテムを使い果たしたとき。
 //
 //     const two: u8 = while (true) break 2 else 0;         // 2
 //     const three: u8 = for ([1]u8{1}) |f| break 3 else 0; // 3
 //
-// If you do not provide an else clause, an empty one will be
-// provided for you, which will evaluate to the void type, which
-// is probably not what you want. So consider the else clause
-// essential when using loops as expressions.
+// else 節を指定しないと、空の else 節が指定され、
+// それは void 型に評価されますが、それはおそらくあなたが望むものではありません。
+// そのため、ループを式として使用する際にはelse節は必須であると考えましょう。
+//
 //
 //     const four: u8 = while (true) {
 //         break 4;
-//     };               // <-- ERROR! Implicit 'else void' here!
+//     };               // <-- エラー! ここに暗黙の'else void'がある!
 //
-// With that in mind, see if you can fix the problem with this
-// program.
+// このことを踏まえて、このプログラムで問題を解決できるかどうか
+// 見てみましょう。
 //
 const print = @import("std").debug.print;
 
@@ -43,8 +43,8 @@ pub fn main() void {
         "Prolog",
     };
 
-    // Let's find the first language with a three-letter name and
-    // return it from the for loop.
+    // 3文字の名前を持つ最初の言語を見つけて 
+    // それをforループから返します。
     const current_lang: ?[]const u8 = for (langs) |lang| {
         if (lang.len == 3) break lang;
     };
